b763fc4c1ba52f16fa4c97a06ac82d0de7fb8da9
Hazhar Maroufi <hazhmar@gmail.com>
2025-04-16 15:54:46 +0330

    done

--- src/HttpReq.jsx
+++ src/HttpReq.jsx
@@ -4,17 +4,39 @@ const initialState = {
   data: [],
   error: "",
 };
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SUCCESS":
+      return { isLoading: false, data: action.payload, error: "" };
+    case "FAILED":
+      return { isLoading: false, data: [], error: action.payload };
+    default:
+      throw new Error("Invalid Action");
+  }
+};
 
 function HttpReq() {
   const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((error) => console.log(error.message));
+      .then((json) => dispatch({ type: "SUCCESS", payload: json }))
+      .catch((error) => dispatch({ type: "FAILED", payload: error.message }));
   }, []);
-  return <div>HttpReq</div>;
+  return (
+    <div>
+      {state.isLoading && <p>Loading...</p>}
+      <div>
+        {" "}
+        <ul>
+          {state.data.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+      {!!state.error && <p>{state.error}</p>}
+    </div>
+  );
 }
 
 export default HttpReq;
